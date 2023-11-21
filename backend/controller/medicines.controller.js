a78abd3d79b122e8285740b91091b3b047d1f49f
inkosator <kane.beek@yandex.ru>
2023-11-21 13:12:25 +0300

    отредактировал поля в бд и контроллере таблицы `Medicines` (type,edc)

--- backend/controller/medicines.controller.js
+++ backend/controller/medicines.controller.js
@@ -4,9 +4,9 @@ class MedicinesController {
 
     //Создание юзера
     async createPost(req, res) {
-        const {name, title, dozimitr, instruction, id_users, id_reminders, id_shedules} = req.body
-        const newMedicines = await db.query(`INSERT INTO medicines (name, title, dozimitr, instruction, id_users, id_reminders, id_shedules) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [name, title, dozimitr, instruction, id_users, id_reminders, id_shedules])
-        console.log(name, title, dozimitr, instruction, id_users, id_reminders, id_shedules)
+        const {name, type, dozimitr, edc, id_users, id_reminders, id_shedules} = req.body
+        const newMedicines = await db.query(`INSERT INTO medicines (name, type, dozimitr, edc, id_users, id_reminders, id_shedules) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`, [name, type, dozimitr, edc, id_users, id_reminders, id_shedules])
+        console.log(name, type, dozimitr, edc, id_users, id_reminders, id_shedules)
         res.json(newMedicines.rows[0])
     }
 
@@ -26,9 +26,9 @@ class MedicinesController {
 
     // Обновить Лекарства
     async updateMedicicnes(req,res) {
-        const {id, name, title, dozimitr, instruction, id_users, id_reminders, id_shedules} = req.body
-        const medicine = await db.query('UPDATE medicines set name = $1, title = $2, dozimitr = $3, instruction = $4, id_users = $5, id_reminders = $6, id_shedules = $7 where id = $8 RETURNING *',
-            [name, title, dozimitr, instruction, id_users, id_reminders, id_shedules, id]
+        const {id, name, type, dozimitr, edc, id_users, id_reminders, id_shedules} = req.body
+        const medicine = await db.query('UPDATE medicines set name = $1, type = $2, dozimitr = $3, edc = $4, id_users = $5, id_reminders = $6, id_shedules = $7 where id = $8 RETURNING *',
+            [name, type, dozimitr, edc, id_users, id_reminders, id_shedules, id]
         )
         res.json(medicine.rows[0])
     }
